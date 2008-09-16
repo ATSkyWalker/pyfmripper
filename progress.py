038cfd9cb22dae9768f6900e4e4959b9b862c66d
Ahmed Talaat <ahmedt@rogers.com>
2008-09-16 09:29:33 -0400

    Minor documentation and license changes before releasing it into the wild

--- progress.py
+++ progress.py
@@ -3,8 +3,24 @@
 """
 progress.py
 
-Created by Ahmed Talaat on 2008-09-12.
-Copyright (c) 2008 Spade Consulting Inc.. All rights reserved.
+Created by Ahmed Talaat on 2008-09-11.
+Copyright (c) 2008 Ahmed Talaat.
+
+
+This file is part of PyFMRipper.
+
+PyFMRipper is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+PyFMRipper is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with PyFMRipper.  If not, see <http://www.gnu.org/licenses/>.
 """
 
 import sys
